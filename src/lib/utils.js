2a5783c52e42a08be200091ec25072cb06c13d2a
Sharad <sharad011989@gmail.com>
2025-02-20 00:23:11 +0530

    add all

---
+++ src/lib/utils.js
@@ -0,0 +1,21 @@
+const COLORS = {
+    green: {
+        bg: "bg-[#ECF7D4]",
+        badge: "bg-[#D6F497]"
+    },
+    orange: {
+        bg: "bg-[#FDEDD4]",
+        badge: "bg-[#FFC107]",
+    },
+    red: {
+        bg: "bg-[#FDDDDD]",
+        badge: "bg-[#F44336]",
+    },
+};
+
+export const getBadgeColor = () => {
+    const colorNames = Objects.keys(COLORS);
+    const randomIndex = Math.floor(Math.random() * colorNames.length);
+    const randomColorName = colorNames[randomIndex];
+    return COLORS[randomColorName];
+}
